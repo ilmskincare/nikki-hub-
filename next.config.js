3fc2a576a6527c6438685202cc1c585e49401166
ilmskincare <125162858+ilmskincare@users.noreply.github.com>
2026-02-19 16:19:45 +0100

    Fix: middleware env var + force dynamic API routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    APP_PASSWORD: process.env.APP_PASSWORD,
+  },
 };
 
 module.exports = nextConfig;
